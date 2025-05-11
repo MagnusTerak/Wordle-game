60210019ae22ddcc1e0d2d0e1b294d6d88f3e381
Magnus <49763392+MagnusTerak@users.noreply.github.com>
2025-05-11 23:31:44 +0200

    added backend call to retrieve wordlist from github API

--- backend/static/main.js
+++ backend/static/main.js
@@ -1,8 +1,38 @@
-console.log("test")
+const wordList = "https://raw.githubusercontent.com/dwyl/english-words/master/words_alpha.txt"
 
-async function getRandomNumber() {
-  const response = await fetch("./public/words_alpha.txt");
-  const payLoad = response.json();
+async function getWordList() {
+  try {
+    const response = await fetch(wordList)
 
-  console.log("payload:", payLoad);
+    if (response.ok) {
+      const data = await response.text()
+      const words = data.split("\n").map((word) => word.trim())
+
+      return words
+    } else {
+      console.error("Couldn't fetch words from wordlist")
+    }
+  } catch (error) {
+    console.error("Couldn't fetch words from wordlist")
+    return []
+  }
 }
+
+async function getWordFromSettings(data) {
+  const words = await getWordList();
+
+  let filteredWords = words.filter(word => word.length === data.length);
+
+  if (data.uniqueLetters) {
+    filteredWords = filteredWords.filter(currentWord => new Set(currentWord).size === currentWord.length);
+  }
+
+  let randomIndex = Math.floor(Math.random() * filteredWords.length);
+  let word = filteredWords[randomIndex]
+
+  return word
+}
+
+
+
+export default getWordFromSettings
