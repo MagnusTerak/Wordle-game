1bb483286d540f2449bbd1e715c31fbce030ac30
Magnus <49763392+MagnusTerak@users.noreply.github.com>
2025-05-11 23:31:17 +0200

    added SSR for site and serving site at port 5080 with node

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,35 @@
 import express from "express";
+import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
+import getWordFromSettings from "./static/main.js";
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send('<html><body><script src="/static/main.js"></script></body></html>');
-})
+app.use(cors());
+
+app.set("view engine", "ejs")
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+
+app.post("/api/getWord", express.json(), async (req, res) => {
+  const { length, uniqueLetters } = req.body;
+
+  const wordData = { length: Number(length), uniqueLetters };
+  const randomWord = await getWordFromSettings(wordData);
+
+  res.json({ word: randomWord });
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/dist', 'index.html'));
+});
+
+
 
 app.use("/static", express.static("static"));
 
